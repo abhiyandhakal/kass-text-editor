18f81dcb0d258e4e9ef291f1eee68a9c3437dced
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2023-05-01 10:43:22 +0545

    terminal doesn't clear when opening kass

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -1,50 +1,37 @@
 use crossterm::{cursor, execute, terminal};
-use std::{
-    env::args,
-    io::{stdout, Result},
-};
+use std::io::{stdout, Result};
 
+mod enums;
+mod filetree;
 mod kass;
-mod mode;
 mod statusbar;
+mod window;
 
 fn main() -> Result<()> {
     // move cursor to 0,0
     // enter alternate screen
+
     execute!(
         stdout(),
-        cursor::MoveTo(0, 0),
-        terminal::EnterAlternateScreen
+        cursor::SavePosition,
+        terminal::EnterAlternateScreen,
+        cursor::MoveTo(0, 0)
     )?;
     // enable raw mode
     terminal::enable_raw_mode()?;
 
-    // get terminal size
-    let mut height: usize = 0;
-    let mut width: usize = 0;
-    if let Some((w, h)) = term_size::dimensions() {
-        height = h as usize;
-        width = w as usize
-    } else {
-        print!("Unable to get term size :(\n")
-    }
-
-    // get file path
-    let args: Vec<String> = args().collect();
-    let mut filepath: &String = &String::from("n/a");
-
-    if args.len() > 1 {
-        filepath = &args[1];
-    }
-
     // text editor
-    let mut kass = kass::Kass::new(height, width, filepath)?;
+    let mut kass = kass::Kass::new()?;
     kass.run()?;
 
     // disable raw mode
     terminal::disable_raw_mode()?;
     // leave alternate screen
-    execute!(stdout(), terminal::LeaveAlternateScreen)?;
+    execute!(
+        stdout(),
+        cursor::RestorePosition,
+        terminal::LeaveAlternateScreen
+    )?;
 
     Ok(())
 }
